dafe8af6979a65d4ff92e590dab06233fe08c007
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-02-14 23:48:30 +0530

    refresh-token route or endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser, logoutUser } from "../controllers/user.constrollers.js";
+import { registerUser,loginUser, logoutUser,refreshAccessToken } from "../controllers/user.constrollers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const userRouter=Router()
@@ -17,6 +17,8 @@ userRouter.route("/login").post(loginUser)
 
 
 //secured Routes
-userRouter.route("/logout").post(verifyJWT,logoutUser)
+userRouter.route("/logout").post(verifyJWT,logoutUser);
+userRouter.route("/refres-token").post(refreshAccessToken)
+
 
 export default userRouter
